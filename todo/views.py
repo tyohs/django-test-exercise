1b2861b425dd14b8c8b2d0687fd7d0aa64bb9124
1F10250148 <s1f102501484@iniad.org>
2026-07-02 13:57:55 +0900

    終了機能の追加

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import Http404
 from django.utils.dateparse import parse_datetime
 from django.utils.timezone import make_aware
@@ -33,3 +33,12 @@ def detail(request, task_id):
         'task': task,
     }
     return render(request, 'todo/detail.html', context)
+
+def close(request, task_id):
+    try:
+        task = Task.objects.get(pk=task_id)
+    except Task.DoesNotExist:
+        raise Http404("Task does not exist")
+    task.completed = True
+    task.save()
+    return redirect(index)
