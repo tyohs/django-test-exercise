6e3e77d64928af7e3873efb714d24383709fd8cf
1F10250148 <s1f102501484@iniad.org>
2026-07-16 13:48:22 +0900

    リアクション機能を追加

--- config/urls.py
+++ config/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('', todo_views.index, name='index'),
     path('<int:task_id>/', todo_views.detail, name='detail'),
     path('<int:task_id>/close/', todo_views.close, name='close'),
+    path('<int:task_id>/react/', todo_views.react, name='react'),
     path('<int:task_id>/update', todo_views.update, name='update'),
     path('<int:task_id>/delete', todo_views.delete, name='delete'),
 ]
--- todo/models.py
+++ todo/models.py
@@ -7,6 +7,9 @@ class Task(models.Model):
     completed = models.BooleanField(default=False)
     posted_at = models.DateTimeField(default=timezone.now)
     due_at = models.DateTimeField(null=True, blank=True)
+    like_count = models.IntegerField(default=0)
+    love_count = models.IntegerField(default=0)
+    wow_count = models.IntegerField(default=0)
 
     def is_overdue(self, dt):
         if self.due_at is None:
